acae40ef548c6c13268ec676d4b8d3abb2272529
Mut Tulow <mtulow@icloud.com>
2024-10-19 03:45:01 -0600

    Identify relevant tables and columns for sub-query.

--- sql/top_artists_by_country.sql
+++ sql/top_artists_by_country.sql
@@ -1,50 +1,26 @@
--- Top artists by country
-WITH ArtistPerformance AS (
-    SELECT
-        /* Rank artists columns */
-        rank() OVER
-            (PARTITION BY c.Country ORDER BY i.InvoiceDate DESC) AS ArtistRank,
-        
-        /* Artist columns */
-        ar.ArtistId,
-        ar.Name AtristName,
-
-        /* Customer columns */
-        c.Country,
-
-        /* Aggregate columns */
-        SUM(il.Quantity) OVER
-            (PARTITION BY c.Country ORDER BY i.InvoiceDate) Purchases,
-        SUM(il.UnitPrice*il.Quantity) OVER
-            (PARTITION BY c.Country ORDER BY i.InvoiceDate) Sales
-        
-    FROM InvoiceLine il
-    JOIN Track t ON il.TrackId = t.TrackId
-    JOIN Album al ON t.AlbumId = al.AlbumId
-    JOIN Artist ar ON al.ArtistId = ar.ArtistId
-    JOIN Invoice i ON il.InvoiceId = i.InvoiceId
-    JOIN Customer c ON i.CustomerId = c.CustomerId
-    GROUP BY c.Country, ar.ArtistId, ar.Name
-)
+/* Ranked artists per country */
+-- SUB-QUERY: Identify relevant tables and their columns
 SELECT
-    -- /* Rank column */
-    -- ap.ArtistRank,
-    
-    /* Artist columns */
-    ap.ArtistId,
-    ap.AtristName,
+    /* InvoiceLine Columns */
+    il.InvoiceLineId,
+    il.Quantity,
+    il.UnitPrice,
 
-    /* Customer columns */
-    ap.Country,
+    /* Artist Columns */
+    ar.ArtistId,
+    ar.Name,
 
-    /* Aggregate columns */
-    ap.Purchases ArtistPurchases,
-    ROUND(ap.Sales, 2) ArtistSales
+    /* Invoice Columns */
+    DATE(i.InvoiceDate) InvoiceDate,
+    i.BillingCountry,
 
-FROM ArtistPerformance ap
-JOIN (
-    SELECT Country, MAX(Purchases) AS MaxPurchases
-    FROM ArtistPerformance
-    GROUP BY Country
-) mp ON ap.Country = mp.Country AND ap.Purchases = mp.MaxPurchases
-;
+    /* Customer Columns */
+    c.CustomerId
+
+FROM InvoiceLine il
+JOIN Track t ON il.TrackId = t.TrackId
+JOIN Album al ON t.AlbumId = al.AlbumId
+JOIN Artist ar ON al.ArtistId = ar.ArtistId
+JOIN Invoice i ON il.InvoiceId = i.InvoiceId
+JOIN Customer c ON i.CustomerId = c.CustomerId;
+
