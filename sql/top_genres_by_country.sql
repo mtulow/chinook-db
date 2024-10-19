65820d997c639ecc4be881e039ac7730c831175e
Mut Tulow <mtulow@icloud.com>
2024-10-19 00:19:24 -0600

    Successfully produced genre table.

--- sql/top_genres_by_country.sql
+++ sql/top_genres_by_country.sql
@@ -1,54 +1,38 @@
--- Top genre per country
+-- Top genres per country
 WITH GenrePerformance AS (
-    SELECT 
-        /* Rank the columns */
-        rank() OVER
-            (PARTITION BY c.Country ORDER BY i.InvoiceDate DESC) AS GenreRank,
-        
+    SELECT
         /* Genre columns */
         g.GenreId,
         g.Name,
-
+        
         /* Customer columns */
         c.Country,
-
+        
         /* InvoiceLine columns */
-        SUM(il.Quantity) OVER
-            (PARTITION BY c.Country ORDER BY i.InvoiceDate) AS Purchases,
-        SUM(il.Quantity*il.UnitPrice) OVER
-            (PARTITION BY c.Country ORDER BY i.InvoiceDate) AS Sales
-    
+        COUNT(il.InvoiceLineId) AS Purchases,
+        SUM(il.Quantity*il.UnitPrice) AS Sales
     FROM InvoiceLine il
     JOIN Invoice i ON il.InvoiceId = i.InvoiceId
     JOIN Customer c ON i.CustomerId = c.CustomerId
     JOIN Track t ON il.TrackId = t.TrackId
     JOIN Genre g ON t.GenreId = g.GenreId
+    GROUP BY c.Country, g.GenreId, g.Name
 )
 SELECT
-    /* Rank column */
-    gp.GenreRank,
-    
-    /* Genre Columns */
+    /* Genre columns */
     gp.GenreId,
-    gp.Name GenreName,
-    
+    gp.Name Genre,
+
     /* Location columns */
     gp.Country,
 
     /* Aggregation columns */
     gp.Purchases,
-    gp.Sales
+    ROUND(gp.Sales, 2) Sales
 
 FROM GenrePerformance gp
 JOIN (
-    SELECT
-        Country,
-        MAX(Purchases) AS MaxPurchases,
-        MAX(Sales) AS MaxSales
+    SELECT Country, MAX(Purchases) AS MaxPurchases
     FROM GenrePerformance
     GROUP BY Country
-) mp ON gp.Country = mp.Country
-    AND gp.Purchases = mp.MaxPurchases
-    -- AND gp.Sales = mp.MaxSales
-WHERE GenreRank <= 1
-;
+) mp ON gp.Country = mp.Country AND gp.Purchases = mp.MaxPurchases;
