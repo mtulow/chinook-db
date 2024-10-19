d1fb8452153b578262626d58eeeee3b5e5d81f90
Mut Tulow <mtulow@icloud.com>
2024-10-19 00:36:14 -0600

    top employees sub-query successful.

--- sql/top_performing_employees.sql
+++ sql/top_performing_employees.sql
@@ -1,26 +1,12 @@
-
+/* SUB-QUERY: Top employees by month */
+-- SELECT
+--     *
+-- FROM Invoice;
 SELECT
-    /* Rank column */
-    
-    /* Employee columns */
-    e.EmployeeId,
-    e.FirstName,
-    e.LastName,
-    
-    /* Date column */
-    i.InvoiceDate,
-    STRFTIME('%Y', i.InvoiceDate) Year,
-    STRFTIME('%m', i.InvoiceDate) Month,
-    
-    /* Customer columns */
-    COUNT(c.CustomerId) OVER
-        (PARTITION BY e.EmployeeId ORDER BY i.InvoiceDate) AS TotalCustomers,
-    
-    /* Invoice columns */
-    SUM(i.Total) OVER
-        (PARTITION BY e.EmployeeId ORDER BY i.InvoiceDate) AS TotalSales
+    InvoiceId,
+    CustomerId,
+    DATE(InvoiceDate) InvoiceDate,
+    Total
+FROM Invoice;
 
-FROM Invoice i
-JOIN Customer c ON i.CustomerId = c.CustomerId
-JOIN Employee e ON c.SupportRepId = e.EmployeeId;
 
