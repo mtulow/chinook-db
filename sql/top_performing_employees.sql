dac3c29b1ec8cefb5168ed1d258d395373e05a3b
Mut Tulow <mtulow@icloud.com>
2024-10-19 02:24:20 -0600

    Aggregate combined table by year, month, and employee .

--- sql/top_performing_employees.sql
+++ sql/top_performing_employees.sql
@@ -1,21 +1,24 @@
-/* Combine into single table: 412 rows */
-
+/* Aggregate combined table by year, month, and employee */
+-- GROUP BY table: 412 -> 171 rows
 SELECT
     /* Invoice Columns */
     i.InvoiceId,
-    i.Total InvoiceTotal,
+    -- i.Total InvoiceTotal,
     DATE(i.InvoiceDate) InvoiceDate,
-    STRFTIME('%m', i.InvoiceDate) InvoiceMonth,
+    STRFTIME('%Y', i.InvoiceDate) Year,
+    STRFTIME('%m', i.InvoiceDate) Month,
 
-    /* Customer Columns */
-    c.CustomerId,
-    
     /* Employee Columns */
     e.EmployeeId,
     e.LastName EmployeeLastName,
     e.FirstName EmployeeFirstName,
-    e.Title EmployeeTitle
+
+    /* Customer Columns */
+    COUNT(c.CustomerId) TotalCustomers,
+    SUM(i.Total) TotalSales
 
 FROM Invoice i
 JOIN Customer c ON i.CustomerId = c.CustomerId
-JOIN Employee e ON c.SupportRepId = e.EmployeeId;
+JOIN Employee e ON c.SupportRepId = e.EmployeeId
+GROUP BY Year, Month, e.EmployeeId
+ORDER BY i.InvoiceDate;
